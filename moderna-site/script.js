837ca0232d237b4a98c3a181efbb50cba4c910f9
ceaumaalle-ui <ceaumaalle@gmail.com>
2025-10-11 15:22:23 -0300

    Delete moderna-site/script.js

--- moderna-site/script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scroll para navegação suave
-document.querySelectorAll('.nav a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const alvo = document.querySelector(this.getAttribute('href'));
-    alvo.scrollIntoView({ behavior: 'smooth' });
-  });
-});
